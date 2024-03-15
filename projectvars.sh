bfd0eb450148b3d3691e69fe693c6d1fc5fb0eef
ameliapapa <53392154+ameliapapa@users.noreply.github.com>
2024-03-15 09:35:43 +0100

    Add files via upload

---
+++ projectvars.sh
@@ -0,0 +1,69 @@
+## ######################################################################################
+##
+##   projectvars.sh
+##
+##   A BASH initialization file for MScGG-BIA practical exercise folders
+##
+## ######################################################################################
+##
+##                 CopyLeft 2023/24 (CC:BY-NC-SA) --- Josep F Abril
+##
+##   This file should be considered under the Creative Commons BY-NC-SA License
+##   (Attribution-Noncommercial-ShareAlike). The material is provided "AS IS", 
+##   mainly for teaching purposes, and is distributed in the hope that it will
+##   be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+##   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+##
+## ######################################################################################
+
+#
+# Base dir
+export WDR=$PWD; # IMPORTANT: make sure that your path DOES NOT contains white-spaces
+                 #            otherwise, you will get weird execution errors.
+                 #            If you cannot fix the dir names containing such white-space
+                 #            chars, you MUST set this var using the current folder '.'
+                 #            instead of '$PWD', i.e:    export WDR=.;
+export BIN=$WDR/bin;
+export DOC=$WDR/docs;
+
+#
+# Formating chars
+export TAB=$'\t';
+export RET=$'\n';
+export LC_ALL="en_US.UTF-8";
+
+#
+# pandoc's vars
+NM="SURNAME_NAME";                    #-> IMPORTANT: SET YOUR SURNAME and NAME ON THIS VAR,
+RB="README_MScGG-BIA2324_exercise02"; #->            MUST FIX ON MARKDOWN README FILE
+                                      #->            FROM TARBALL (AND INSIDE TOO)
+RD="${RB}_${NM}";
+PDOCFLGS='markdown+pipe_tables+header_attributes';
+PDOCFLGS=$PDOCFLGS'+raw_tex+latex_macros+tex_math_dollars';
+PDOCFLGS=$PDOCFLGS'+citations+yaml_metadata_block';
+PDOCTPL=$DOC/MScGG_BIA_template.tex;
+export RD PDOCFLGS PDOCTPL;
+
+function ltx2pdf () {
+    RF=$1;
+    pdflatex $RF.tex;
+    bibtex $RF;
+    pdflatex $RF.tex;
+    pdflatex $RF.tex;
+}
+
+function runpandoc () {
+  pandoc -f $PDOCFLGS               \
+         --template=$PDOCTPL        \
+         -t latex --natbib          \
+         --number-sections          \
+         --highlight-style pygments \
+         -o $RD.tex $RD.md;
+  ltx2pdf $RD;
+}
+
+#
+# add your bash defs/aliases/functions below...
+alias dir='/bin/ls -alFhrt --color=auto'
+
+export DT=$WDR/data;
